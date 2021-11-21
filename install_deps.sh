7cb4072437a9b3365e8a071ab3bf66b9625db1be
Ivan Khokhlov <khokhlov.iyu@gmail.com>
2021-11-21 22:47:34 +0300

    Develop (#12) Indroducing working vision, motion and model modules
    * Adding motion package (#9)
    
    * motion added
    
    * cleaning
    
    Co-authored-by: Ivan Khokhlov <khokhlov.iyu@gmail.com>
    
    * Moved motion pkg to core, fixed some import problems, cleaned
    
    * Delete CMakeLists.txt
    
    * Added partially implemented vision_node.py; added missing filter; added config reading(in progress)
    
    * Added vision config for ball detection
    
    * UPD: Added vision node and launch file, fixed vision lib
    
    Co-authored-by: Aleksandr Matsun <ienjoyeatingcorpses@gmail.com>
    Co-authored-by: HiTF <ryakin.is@phystech.edu>

--- install_deps.sh
+++ install_deps.sh
@@ -1,3 +1,4 @@
 sudo apt-get install v4l-utils
+sudo apt-get install python3-empy
 sudo apt install ros-$ROS_DISTRO-libuvc-ros
-
+pip3 install pyserial
--- src/core/nodes/vision_node.py
+++ src/core/nodes/vision_node.py
@@ -2,23 +2,69 @@
 
 import numpy as np
 import cv2
+import json
 
-from sensor_msgs.msg import CameraInfo, Image, Point
+from sensor_msgs.msg import CameraInfo, Image
+from geometry_msgs.msg import Point
+
 import message_filters
 import rospy
+import importlib
 
-from pipeline import Pipeline
+from common.module import Module
+#from pipeline import Pipeline
 import vision
 
+class VisionPipeline(Module):
+    def __init__(self, params, image_publisher):
+        super().__init__(params)
+        self.show = True
+        self.filters = []
+        self.res_pub = image_publisher
+        self.load_configuration(params["processors"])
+
+    def apply(self, inp):
+        #read, process, write
+        print("Processing...")
+        rgb_image = np.frombuffer(inp.data, dtype=np.uint8).reshape(inp.height, inp.width, -1)
+        results = [rgb_image]
+
+        for f in self.filters:
+            results.append(f.apply(results[-1]))
+
+        bb, success = results[-1]
+        if success:
+            self.res_pub.publish(self.get_point_from_bb(bb))
+        # return results[-1]
+
+    @staticmethod
+    def get_point_from_bb(bb):
+        (left, _), (right, bottom) = bb
+        return Point(left + right, bottom, 0) 
+
+    def load_configuration(self, vision_module_config):
+        self.vision_module_name = vision_module_config["name"]
+
+        vision_library = importlib.import_module("vision.filters")
+        
+        for filter_params in vision_module_config["filters"]:
+            #Creating instance of module
+            module_name = filter_params["name"]
+            module_instance = getattr(vision_library, module_name)
+            module = module_instance(filter_params)
+            self.filters.append(module)
+            print("Module \033[92m{}\033[0m added to container".format(module_name))
+
 if __name__ == '__main__':
-    rospy.init_node('vision_node', anonymous=True)
+    vision_config_path = rospy.get_param("vision_config")
+    with open(vision_config_path) as f:
+        vision_config = json.load(f)
+    rospy.init_node(vision_config["node_name"])
+    # print("VISION_CONFIG: ", vision_config)
+    image_publisher = rospy.Publisher(vision_config["pub_topic_name"], Point, queue_size=10)
+    pipeline = VisionPipeline(vision_config, image_publisher)
 
-    image_sub = message_filters.Subscriber('/usb_cam/image_raw', Image)
-    info_sub = message_filters.Subscriber('/usb_cam/camera_info', CameraInfo)
-
-    pipeline = Pipeline()
-
-    ts = message_filters.ApproximateTimeSynchronizer([image_sub, info_sub], 10, 0.2)
-    ts.registerCallback(pipeline.callback)
+    image_sub = rospy.Subscriber('/usb_cam/image_raw', Image, pipeline.apply)
 
     rospy.spin()
+
